25dbe0dbebfe4ba5c3d2062abb5b0313f9c1392f
NotLebedev <artem-itf@yandex.ru>
2020-12-07 15:56:06 +0300

    +Finished RK 4 for multiple equations

--- RK.c
+++ RK.c
@@ -4,7 +4,7 @@
 #include "RK.h"
 
 double *rk_single(f2 func, Order order, double x0, double y0, double l, size_t n) {
-    // Создаём массив, содержащий значения по y для равномерной сетки от x0 до x0 + l с шагом l/n
+    // Создаём массив, содержащий значения по x для равномерной сетки от x0 до x0 + l с шагом l/n
     double *res = calloc(n + 1, sizeof(*res));
     if (res == NULL) {
         return NULL;
@@ -67,7 +67,20 @@ Vector2D *rk_double(f3 func1, f3 func2, Order order, double t0, double x0, doubl
             variation_x = (k1 + k2) * step / 2.0;
             variation_y = (l1 + l2) * step / 2.0;
         } else {
+            const double k1 = func1(t_prev, x_prev, y_prev);
+            const double l1 = func2(t_prev, x_prev, y_prev);
 
+            const double k2 = func1(t_prev + step / 2.0, x_prev + step * k1 / 2.0, y_prev + step * l1 / 2.0);
+            const double l2 = func2(t_prev + step / 2.0, x_prev + step * k1 / 2.0, y_prev + step * l1 / 2.0);
+
+            const double k3 = func1(t_prev + step / 2.0, x_prev + step * k2 / 2.0, y_prev + step * l2 / 2.0);
+            const double l3 = func2(t_prev + step / 2.0, x_prev + step * k2 / 2.0, y_prev + step * l2 / 2.0);
+
+            const double k4 = func1(t_prev + step, x_prev + step * k3, y_prev + step * l3);
+            const double l4 = func2(t_prev + step, x_prev + step * k3, y_prev + step * l3);
+
+            variation_x = (k1 + 2.0 * k2 + 2.0 * k3 + k4) * step / 6.0;
+            variation_y = (l1 + 2.0 * l2 + 2.0 * l3 + l4) * step / 6.0;
         }
 
         res[i + 1].x = x_prev + variation_x;
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 #include "RK.h"
 
@@ -6,11 +7,27 @@ double f(double x, double y) {
     return - y / x;
 }
 
-int main() {
+double g1(double t, double x, double y) {
+    return t + x - y * y + 2;
+}
+
+double g2(double t, double x, double y) {
+    return sin(t - x) + 2.1 * y;
+}
+
+int main(void) {
     double *res = rk_single(f, RK_4, 1.0, 3.0, 5.0, 30);
 
-    for (size_t i = 0; i < 31; i++) {
-        printf("%lf %lf\n", 1.0 + i * (5.0 / 30), res[i]);
+    Vector2D *res2 = rk_double(g1, g2, RK_4, 0, 1.5, 0, 5.0, 300);
+
+    for (size_t i = 0; i < 301; i++) {
+        //printf("%lf %lf\n", 1.0 + i * (5.0 / 30), res[i]);
+        printf("{%lf, %lf}, ", i * (5.0 / 300), res2[i].x);
+    }
+    printf("\n");
+    for (size_t i = 0; i < 301; i++) {
+        //printf("%lf %lf\n", 1.0 + i * (5.0 / 30), res[i]);
+        printf("{%lf, %lf}, ", i * (5.0 / 300), res2[i].y);
     }
     return 0;
 }
