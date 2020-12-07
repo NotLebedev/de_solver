b17e06d36dddf5d19f0c306784afa88c8ef82eee
NotLebedev <artem-itf@yandex.ru>
2020-12-07 15:25:23 +0300

    +RK for two equations

--- RK.c
+++ RK.c
@@ -37,3 +37,41 @@ double *rk_single(f2 func, Order order, double x0, double y0, double l, size_t n
     }
     return res;
 }
+
+Vector2D *rk_double(f3 func1, f3 func2, Order order, double t0, double x0, double y0, double l, size_t n) {
+    Vector2D *res = calloc(n + 1, sizeof(*res));
+    if (res == NULL) {
+        return NULL;
+    }
+
+    // Задаём начальное значение
+    res[0].x = x0;
+    res[0].y = y0;
+
+    // Задаём шаг сетки алгоритма
+    const double step = l / n;
+    for (size_t i = 0; i < n; i++) {
+        const double t_prev = t0 + i * step;
+        const double x_prev = res[i].x;
+        const double y_prev = res[i].y;
+
+        // Представим результат в новом узле в виде x_{i+1} = x_i + variation_x, variation_x вычислим в зависимости от
+        // выбранного порядка и аналогично для y
+        double variation_x;
+        double variation_y;
+        if (order == RK_2) {
+            const double k1 = func1(t_prev, x_prev, y_prev);
+            const double l1 = func2(t_prev, x_prev, y_prev);
+            const double k2 = func1(t_prev + step, x_prev + step * k1, y_prev + step * l1);
+            const double l2 = func2(t_prev + step, x_prev + step * k1, y_prev + step * l1);
+            variation_x = (k1 + k2) * step / 2.0;
+            variation_y = (l1 + l2) * step / 2.0;
+        } else {
+
+        }
+
+        res[i + 1].x = x_prev + variation_x;
+        res[i + 1].y = y_prev + variation_y;
+    }
+    return res;
+}
--- RK.h
+++ RK.h
@@ -2,7 +2,12 @@
 #define DE_SOLVER_RK_H
 
 typedef double (*f2)(double, double);
-typedef double (*f3)(double, double);
+typedef double (*f3)(double, double, double);
+
+typedef struct {
+    double x;
+    double y;
+} Vector2D;
 
 typedef enum {
     RK_2,
@@ -10,6 +15,6 @@ typedef enum {
 } Order;
 
 double *rk_single(f2 func, Order order, double x0, double y0, double l, size_t n);
-double *rk_double(f3 func, Order order, double x0, double y0, double l, double n);
+Vector2D *rk_double(f3 func1, f3 func2, Order order, double t0, double x0, double y0, double l, size_t n);
 
 #endif //DE_SOLVER_RK_H
