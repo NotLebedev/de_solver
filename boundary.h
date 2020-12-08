cc21224c862ecb3477810adc19a67727ef0f426d
artemiy <artem-itf@yandex.ru>
2020-12-08 09:57:57 +0300

    +Defined function for solving boundary problem

---
+++ boundary.h
@@ -0,0 +1,11 @@
+#ifndef DE_SOLVER_BOUNDARY_H
+#define DE_SOLVER_BOUNDARY_H
+
+#include <stddef.h>
+#include "RK.h"
+
+typedef double (*f1)(double);
+
+double *boundary(double x_0, double x_n, f1 eq_coef[3], Vector2D init_coef[3], size_t n);
+
+#endif //DE_SOLVER_BOUNDARY_H
